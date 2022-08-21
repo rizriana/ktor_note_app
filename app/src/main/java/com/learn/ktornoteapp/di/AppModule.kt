8f2c73394cfedcb8a0aebe1b0d346bfb92d1ab51
Rizki Rian Anandita <65359346+rizriana@users.noreply.github.com>
2022-08-21 23:58:08 +0700

    encrypt local database

--- app/src/main/java/com/learn/ktornoteapp/di/AppModule.kt
+++ app/src/main/java/com/learn/ktornoteapp/di/AppModule.kt
@@ -13,6 +13,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -36,11 +38,17 @@ object AppModule {
     @Provides
     fun provideNoteDatabase(
         @ApplicationContext context: Context,
-    ): NoteDatabase = Room.databaseBuilder(
-        context,
-        NoteDatabase::class.java,
-        "note_db"
-    ).build()
+    ): NoteDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("note_app".toCharArray())
+        val factory = SupportFactory(passphrase)
+        return Room.databaseBuilder(
+            context,
+            NoteDatabase::class.java,
+            "note_db"
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
+    }
 
     @Singleton
     @Provides
